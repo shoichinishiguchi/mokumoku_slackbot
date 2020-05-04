581c67c34d1e5804c800363ca23029c938e1fa98
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-05-04 18:19:45 +0900

    first_commit

---
+++ mokumoku.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'slack-ruby-client'
+
+Slack.configure do |conf|
+  conf.token = ENV['MOKUMOKU_BOT_TOKEN']
+end
+
+# RTM Clientのインスタンス生成
+client = Slack::RealTime::Client.new
+
+# slackに接続できたときの処理
+client.on :hello do
+  puts 'connected!'
+  client.message channel: 'C0135C2JS0L', text: 'connected!'
+end
+mokumoku_count = 0
+
+# ユーザからのメッセージを検知したときの処理
+client.on :message do |data|
+  mokumoku_count += 1 if data.text&.match(/もくもく/)
+  if data.text&.match(/もくもく(はじめ|開始|始め|再開|スタート)/)
+    client.message channel: data['channel'], text: "<@#{data.user}>, もくもく頑張ってね！"
+  end
+  if data.text&.match(/もくもく(終わり|おわり|終了|エンド)/)
+    client.message channel: data['channel'], text: "<@#{data.user}>, もくもくお疲れ様"
+  end
+  if data.text&.match(/何もくもく?/)
+    client.message channel: data['channel'],
+                   text: "もくもくカウントは #{mokumoku_count} 回です。"
+  end
+end
+
+# Bot start
+client.start!
