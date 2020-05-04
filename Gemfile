2f4d93f3ec93dd4af77d52a2ec07a88c71f84365
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-05-04 18:59:48 +0900

    herokuに必要なファイルを作成

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+ruby '2.6.3'
+gem 'slack-ruby-client'
--- other_setting.rb
+++ other_setting.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
 
+#  ここにメソッドを書けば、新しい設定ができます。
 def ura_setting(client)
   client.on :message do |data|
     if data.text&.match(/裏設定ファイル/)
-      client.message channel: data['channel'], text: "ようこそ"
+      client.message channel: data['channel'], text: 'ようこそ'
     end
   end
 end
