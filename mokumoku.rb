78ca111158f38b88aef4af924d0f4d455c201abb
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-09-27 17:21:30 +0900

    feat: キリ番設定

--- mokumoku.rb
+++ mokumoku.rb
@@ -13,6 +13,27 @@ MOKUMOKU_CHANNELS = [
   'C0135C2JS0L' # 実験用
 ].freeze
 
+KIRIBAN = [
+  729,
+  777,
+  888,
+  999,
+  1024,
+  1111,
+  1234,
+  2048,
+  2222,
+  3333,
+  4444,
+  4096,
+  5555,
+  6666,
+  7777,
+  8192,
+  8888,
+  9999
+].freeze
+
 # RTM Clientのインスタンス生成
 client = Slack::RealTime::Client.new
 
@@ -35,13 +56,14 @@ client.on :message do |data|
       mokumoku_count = s3_mokumoku_count_up_and_return
 
       if mokumoku_count % 1000 == 0
-        client.message channel: data['channel'], text: ":congratulations: おめでとうございます！:congratulations:\n:tada:1000回目のもくもく:mokumokubot: です！！最高!:laughing::tada:\nみなさん、今後ともよろしくもくもく:mokumokubot: お願いします！"
+        client.message channel: data['channel'], text: ":congratulations: おめでとうございます！:congratulations:\n:tada:#{mokumoku_count}回目のもくもく:mokumokubot: です！！最高!:laughing::tada:\nみなさん、今後ともよろしくもくもく:mokumokubot: お願いします！"
       elsif mokumoku_count % 100 == 0
         client.message channel: data['channel'], text: ":tada::tada:<@#{data.user}>:tada::tada:\nおめでとうございます。あなたで、\nちょうど:congratulations: #{mokumoku_count}回目:congratulations: のもくもくです。すごい！!\n:mokumokubot: :mokumokubot: みなさん、これからもよろしくもくもくおねがいします！:mokumokubot::mokumokubot: "
+      elsif KIRIBAN.include?(mokumoku_count)
+        client.message channel: data['channel'], text: "キリ番ゲット!:sunglasses:\nちょうど:confetti_ball: 777回目:confetti_ball: のもくもく:mokumokubot: です！\n<@#{data.user}>もくもく頑張ってね！:mokumokubot::clap:"
       else
         client.message channel: data['channel'], text: "<@#{data.user}>\nすごい！もくもく頑張ってね！:clap:"
       end
-
     end
 
     if data.text&.match(/もくもくカウントは/) && data.user == ENV['MOKUMOKU_ADMIN']
